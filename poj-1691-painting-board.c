e5c548828e8f12823e640caf714d10d22c404379
Alan <alan_t@foxmail.com>
2016-04-25 09:07:51 +0800

    Temp work of poj 1691

--- poj-1691-painting-board.c
+++ poj-1691-painting-board.c
@@ -28,14 +28,40 @@ typedef struct Rect {
 
 struct Rect rects[16];
 
+int ans;
+int n; // number of rectangles for each case
+
+int visit[20];
+int mark[100][100];
+
+// pos: current rect pos 1~N
+// colorCnt: how many color brushes used
+// rectCnt: rectangle count 1~N
+void DFS(int pos, int colorCnt, int rectCnt) {
+
+    if (rectCnt == numofCases) {
+        if (ans > colorCnt) ans = colorCnt;
+        return;
+    }
+
+    visit[pos] = 1;
+
+    int i;
+    for (i = r; i < n; i++) {
+        
+    } 
+}
+
 int main(void) {
 
-    int numofCases;
     int i;
+    int numofCases;
     scanf("%d", &numofCases);
 
     while(numofCases--) {
-        for (i = 0; i < numofCases; i++) {
+        scanf ("%d", &n);
+        memset(mark, 0, sizeof(mark));
+        for (i = 0; i < n; i++) {
             scanf("%d %d %d %d %d",
                     &rects[i].upLy,
                     &rects[i].upLx,
@@ -43,7 +69,4 @@ int main(void) {
                     &rects[i].loRx,
                     &rects[i].color); 
         } 
-    }
-    return 0;
-}
-
+    } return 0; } 
