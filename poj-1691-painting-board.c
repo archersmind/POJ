758d19577ea2a467ad89e200aeaea9b1268bb80c
Alan Wang <alan_t@foxmail.com>
2016-04-25 17:02:54 +0800

    Complete poj 1691

--- poj-1691-painting-board.c
+++ poj-1691-painting-board.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  poj-1691-painting-board.c
  *
- *    Description:  Another DFS porblem
+ *    Description:  Another DFS porblem but with Topological theory
  *
  *        Version:  1.0
  *        Created:  04/23/2016 04:04:24 PM
@@ -32,41 +32,83 @@ int ans;
 int n; // number of rectangles for each case
 
 int visit[20];
-int mark[100][100];
+int deg[20]; // degree from the graph theory
+int map[16][16]; // if connected between two rectangles
 
-// pos: current rect pos 1~N
-// colorCnt: how many color brushes used
-// rectCnt: rectangle count 1~N
-void DFS(int pos, int colorCnt, int rectCnt) {
+void buildMap() {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (rects[i].loRy == rects[j].upLy &&
+                    !(rects[i].upLx > rects[j].loRx
+                        || rects[i].loRx < rects[j].upLx)) {
+                deg[j]++;
+                map[i][j] = 1;
+            }
+        }
+    }
+}
 
-    if (rectCnt == numofCases) {
-        if (ans > colorCnt) ans = colorCnt;
+void DFS(int deep, int sum, int color) {
+
+    //printf ("deep = %d sum = %d color = %d ans = %d and n = %d\n", \
+            deep, sum, color, ans, n);
+    if (sum > ans) return; // pruning
+    if (deep == n) {
+        ans = sum; 
         return;
     }
 
-    visit[pos] = 1;
+    int i,j;
+    for (i = 0; i < n; i++) {
+        if (!visit[i] && deg[i] == 0) {
+            visit[i] = 1; 
+            for (j = 0; j < n; j++) { // delete edge from i to j
+                if (map[i][j])        // then in degree from i to j shoule be --
+                    deg[j]--;
+            }
 
-    int i;
-    for (i = r; i < n; i++) {
-        
-    } 
+            if (rects[i].color == color) {
+                DFS(deep + 1, sum, color);
+            } else {
+                DFS(deep + 1, sum + 1, rects[i].color);
+            }
+
+            // back tracing
+            for (j = 0; j < n; j++) {
+                if (map[i][j])
+                    deg[j]++;
+            }
+            visit[i] = 0;
+        }
+    }
 }
 
 int main(void) {
 
-    int i;
     int numofCases;
     scanf("%d", &numofCases);
 
     while(numofCases--) {
         scanf ("%d", &n);
-        memset(mark, 0, sizeof(mark));
+        memset(deg, 0, sizeof(deg));
+        memset(map, 0, sizeof(map));
+        memset(visit, 0, sizeof(visit));
+
+        int i;
         for (i = 0; i < n; i++) {
             scanf("%d %d %d %d %d",
                     &rects[i].upLy,
                     &rects[i].upLx,
                     &rects[i].loRy,
                     &rects[i].loRx,
-                    &rects[i].color); 
-        } 
-    } return 0; } 
+                    &rects[i].color);
+        }
+        buildMap();
+        ans = 20; // MAX brush count
+        DFS(0, 0, 0);
+        printf ("%d\n", ans);
+    }
+
+    return 0;
+}
